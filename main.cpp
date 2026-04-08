b5e6f305a70411a10a672aad11a35ea670dd9a57
Fabi14 <fabian22191@gmail.com>
2026-04-08 20:34:52 +0200

    add glfw and glad

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
-#include <print>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main()
+
+int main(void)
 {
-    std::println("Hello World!");
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* pWindow = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!pWindow)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(pWindow);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
