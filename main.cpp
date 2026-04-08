ee8ee382e869296c57570d82fae61e4e03080eae
Fabi14 <fabian22191@gmail.com>
2026-04-08 20:55:05 +0200

    print openGL version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <sstream>
+#include <print>
+
+std::string getOpenGLInfo()
+{
+    std::stringstream info;
+    info << "Version: " << glGetString(GL_VERSION);
+    return info.str();
+}
 
 
 int main(void)
@@ -7,6 +16,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
     /* Create a windowed mode window and its OpenGL context */
     GLFWwindow* pWindow = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
@@ -28,6 +41,10 @@ int main(void)
     glfwSetWindowPos(pWindow, 4000, 1200);
 
     glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+
+
+    std::print("{}", getOpenGLInfo());
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
