ac4ee526399aea8ff52fb3fec84c70f8f3409f87
Fabi14 <fabian22191@gmail.com>
2026-04-15 17:45:28 +0200

    delete shader program

--- ShaderProgram.h
+++ ShaderProgram.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <glad/glad.h>
+#include <utility>
 
 class Shader;
 
@@ -7,6 +8,20 @@ class ShaderProgram
 {
 public:
 	ShaderProgram(const Shader& vertexShader, const Shader& fragmentShader);
+	ShaderProgram(const ShaderProgram&) = delete;
+	const ShaderProgram& operator=(const ShaderProgram&) = delete;
+	ShaderProgram(ShaderProgram&& old) noexcept : id{ std::exchange(old.id, 0) }
+	{}
+	const ShaderProgram& operator=(ShaderProgram&& old)noexcept
+	{
+		std::swap(*this, old);
+		return *this;
+	}
+
+	~ShaderProgram()
+	{
+		glDeleteProgram(id);
+	}
 	void bind() const;
 private:
 	GLuint id{ 0 };
